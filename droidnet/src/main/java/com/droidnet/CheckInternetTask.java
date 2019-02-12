dd8ac01e53ddf04f2b2f4f460c7ad5f550f1e270
Job Getabu <getabujob@gmail.com>
2019-02-12 13:44:35 +0300

    added CheckInternetTask

---
+++ droidnet/src/main/java/com/droidnet/CheckInternetTask.java
@@ -0,0 +1,79 @@
+/*
+ *Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.droidnet;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+
+//this async task tries to create a socket connection with google.com. If succeeds then return true otherwise false
+class CheckInternetTask extends AsyncTask<Void, Void, Boolean> {
+
+    private WeakReference<TaskFinished<Boolean>> mCallbackWeakReference;
+
+    CheckInternetTask(TaskFinished<Boolean> callback) {
+        mCallbackWeakReference = new WeakReference<>(callback);
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        try {
+            //parse url. if url is not parsed properly then return
+            URL url;
+            try {
+                url = new URL("https://clients3.google.com/generate_204");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+            //open connection. If fails return false
+            HttpURLConnection urlConnection;
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+            urlConnection.setRequestProperty("User-Agent", "Android");
+            urlConnection.setRequestProperty("Connection", "close");
+            urlConnection.setConnectTimeout(1500);
+            urlConnection.connect();
+            return urlConnection.getResponseCode() == 204 && urlConnection.getContentLength() == 0;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Boolean isInternetAvailable) {
+        TaskFinished<Boolean> callback = mCallbackWeakReference.get();
+        if (callback != null) {
+            callback.onTaskFinished(isInternetAvailable);
+        }
+    }
+}
